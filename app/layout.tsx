bafb66ebc4330af1fd65f3fa43354b8ee4ca9ec9
fabiconcept <favourajokubi@gmail.com>
2024-10-11 10:38:11 +0100

    Design `Find Your Program` section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import "./debugger.css";
+// import "./debugger.css";
 import { aismMetadata } from "@/lib/Metadata";
 
 const geistSans = localFont({

f8fefe76e0a7ace29201c009997387d1a9baf0dc
fabiconcept <favourajokubi@gmail.com>
2024-10-12 09:22:09 +0100

    Update MiniInfoTwo component: add background style to container div

--- app/home-page components/MiniInfoTwo.tsx
+++ app/home-page components/MiniInfoTwo.tsx
@@ -2,7 +2,7 @@ import BubbleText from "../general components/BubbleText";
 import Button from "../general components/Button";
 export default function MiniInfoTwo() {
     return (
-        <div className="grid gap-14 grid-cols-[auto_1fr_auto] w-full px-20 pb-10 items-center">
+        <div className="grid gap-14 grid-cols-[auto_1fr_auto] w-full px-20 py-10 items-center bg-black/5">
             <div className="w-fit">
                 <Button sizeVariation="XL">
                     Join Us

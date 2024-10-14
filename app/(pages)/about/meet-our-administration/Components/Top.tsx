420d379ee599d6b9ce7d850d59a3b359ee1fd1be
fabiconcept <favourajokubi@gmail.com>
2024-10-14 18:38:10 +0100

    Update Top component: change 'h-96 overflow-hidden grid place-items-center' to 'relative h-96 overflow-hidden grid place-items-center', and modify width of paragraph element.

--- app/(pages)/about/meet-our-administration/Components/Top.tsx
+++ app/(pages)/about/meet-our-administration/Components/Top.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Top() {
     return (
-        <div className="withGradient h-96 overflow-hidden grid place-items-center">
+        <div className="withGradient relative h-96 overflow-hidden grid place-items-center">
             <Image
                 src={"https://aism-edu.sirv.com/301617876_512872740529074_7075262364263306893_n_LE_auto_x2.jpg"}
                 alt="AISM Group Photo"
@@ -12,7 +12,7 @@ export default function Top() {
             />
             <div className="absolute mt-20 z-20 text-center text-white">
                 <h1 className="text-6xl uppercase font-semibold">Administration</h1>
-                <p className="pt-3 text-xl w-[30rem]">Providing world-class education and training to shape the future leaders in healthcare and medical research</p>
+                <p className="pt-3 text-xl w-[40rem,70%,20rem]">Providing world-class education and training to shape the future leaders in healthcare and medical research</p>
             </div>
         </div>
     )

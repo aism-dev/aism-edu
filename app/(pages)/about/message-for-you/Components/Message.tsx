3488f556313e3491ab84a8821bbefea089f2aaa4
fabiconcept <favourajokubi@gmail.com>
2024-10-14 10:46:08 +0100

    Update Message component with new content and styling - Add font import to globals.css - Modify page.tsx to remove unnecessary div

--- app/(pages)/about/message-for-you/Components/Message.tsx
+++ app/(pages)/about/message-for-you/Components/Message.tsx
@@ -1,5 +1,25 @@
+import Link from "next/link";
+
 export default function MessageSection() {
     return (
-        <div>Message</div>
+        <div className="p-20 grid gap-5">
+            <p></p>
+            <p>Congratulations on being accepted into American International School of Medicine.</p>
+
+            <p>It is with Great pride that I introduce to you an institution, which has grown over the years into an excellent school that provides medical education and training to meet the growing demands for health and health care professionals worldwide.</p>
+
+            <p>Standing by our mission we strive to provide the most efficient service; and we are committed to the preparation of our students for a lifelong process. To become educators, practitioners, leaders and biomedical researchers who can produce discoverers that enhance the understanding of life processes, leading to disease prevention and curing.</p>
+
+            <p>Our major strength lies in our ability to attract and maintain quality faculty and staff with the experiences necessary to provide the highest standard of training and services required to fulfil our mission.</p>
+
+            <p>At AISM, we offer a very dynamic and culturally diverse learning environment.</p>
+
+            <p>As President, I wish to personally extend the key to gateway of acquiring an excellent career in Medical and Health Care sciences. I am confident that your choice to be associated with AISM will be an experience you will always cherish. I wish you a memorable and earnest journey towards success. <Link href={"https://inspirery.com/colin-wilkinson/"} className="text-themelight">Read more</Link></p>
+
+            <p>Welcome to the American International School of Medicine &mdash; AISM</p>
+
+            <p className="font-medium sign-font mt-10 text-3xl text-theme underline underline-offset-8">Dr. Colin A. Wilkinson</p>
+            <p className="font-bold text-lg uppercase text-themeDark -mt-3">Founder & Chairman</p>
+        </div>
     )
 }
--- app/(pages)/about/message-for-you/page.tsx
+++ app/(pages)/about/message-for-you/page.tsx
@@ -1,9 +1,9 @@
-import TopSection from "./Components/Top";
 import MessageSection from "./Components/Message";
+import TopSection from "./Components/Top";
 
 export default function MessageForYou() {
     return (
-        <div className="h-screen mt-44">
+        <div className="mt-44">
             <TopSection />
             <MessageSection />
         </div>

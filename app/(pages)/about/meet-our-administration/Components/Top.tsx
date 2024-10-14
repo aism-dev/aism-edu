8c1b148796874b996ad40d5036cc7d44116a9191
fabiconcept <favourajokubi@gmail.com>
2024-10-14 12:45:54 +0100

    - Update Top.tsx component with new image and text - Add .withGradientLight class to globals.css

--- app/(pages)/about/meet-our-administration/Components/Top.tsx
+++ app/(pages)/about/meet-our-administration/Components/Top.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 export default function Top() {
     return (
-        <div>Top</div>
+        <div className="withGradient h-96 overflow-hidden grid place-items-center">
+            <Image
+                src={"https://aism-edu.sirv.com/301617876_512872740529074_7075262364263306893_n_LE_auto_x2.jpg"}
+                alt="AISM Group Photo"
+                width={2160}
+                height={1208}
+                className="object-contain object-center w-full h-auto md:-mt-24 pointer-events-none select-none"
+            />
+            <div className="absolute mt-20 z-20 text-center text-white">
+                <h1 className="text-6xl uppercase font-semibold">Administration</h1>
+                <p className="pt-3 text-xl">Providing world-class education and training to shape the future leaders in healthcare and medical research</p>
+            </div>
+        </div>
     )
 }

62eeddd0cebf968df694fe3699a9bd65ddde9505
fabiconcept <favourajokubi@gmail.com>
2024-10-14 10:25:56 +0100

    Add MessageSection component to MessageForYou page

--- app/(pages)/about/message-for-you/page.tsx
+++ app/(pages)/about/message-for-you/page.tsx
@@ -1,9 +1,11 @@
 import TopSection from "./Components/Top";
+import MessageSection from "./Components/Message";
 
 export default function MessageForYou() {
     return (
         <div className="h-screen mt-44">
             <TopSection />
+            <MessageSection />
         </div>
     )
 }

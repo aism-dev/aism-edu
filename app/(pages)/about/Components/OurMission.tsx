474854184e98ac9083de25f59a5983b1d64bd1f4
fabiconcept <favourajokubi@gmail.com>
2024-10-23 20:34:23 +0100

    Implement Responsiveness in OurMission Section on the AboutPage

--- app/(pages)/about/Components/OurMission.tsx
+++ app/(pages)/about/Components/OurMission.tsx
@@ -3,11 +3,11 @@ import { FaDotCircle } from "react-icons/fa";
 
 export default function OurMission() {
     return (
-        <div className="grid grid-cols-2 w-full px-40 py-16 items-center gap-5 bg-black/5">
+        <div className="grid md:grid-cols-2 w-full md:px-40 sm:px-10 px-8 py-16 items-center gap-5 bg-black/5">
             <div>
-                <BubbleText formatClass="text-5xl" text="What is AISM's Mission?" />
+                <BubbleText formatClass="sm:text-5xl text-4xl" text="What is AISM's Mission?" />
             </div>
-            <p className="max-w-[clamp(35rem,40%,20rem)] justify-self-end grid gap-4">
+            <p className="sm:max-w-[clamp(35rem,40%,20rem)] w-full justify-self-end grid gap-4">
                 <span className="grid grid-cols-[1rem_1fr] gap-2"><FaDotCircle className="text-sm text-theme" /> The mission of the AISM is to create and nurture a diverse student body committed to offering internationally recognized opportunities for learning, discovery, and engagement.</span>
                 <span className="grid grid-cols-[1rem_1fr] gap-2 text-lg font-medium"><span></span> AISM was born with a vision and that vision continues to grow the medical school.</span>
                 <span className="grid grid-cols-[1rem_1fr] gap-2 opacity-70"><FaDotCircle className="text-sm text-theme" />To be amongst the most distinguished education and training institutions in the world specialized in Medical Health, Environmental Health and Research and Development.</span>

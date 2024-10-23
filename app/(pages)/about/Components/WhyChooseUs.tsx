21b5611dccb3b13afa586d1bd64eefeed14ac3cc
fabiconcept <favourajokubi@gmail.com>
2024-10-23 20:39:28 +0100

    Implement Responsiveness in WhyChooseUs Section on the AboutPage

--- app/(pages)/about/Components/WhyChooseUs.tsx
+++ app/(pages)/about/Components/WhyChooseUs.tsx
@@ -57,43 +57,43 @@ export default function WhyChooseUs() {
                         className="w-full h-full object-cover object-center"
                     />
                 </div>
-                <div className="min-w-[20rem] flex-1 py-10 px-16">
+                <div className="min-w-[20rem] flex-1 py-10 sm:px-16 px-8">
                     <h3 className="text-4xl font-semibold">Why Choose AISM</h3>
                     <p className="max-w-[85%] mt-5 text-themeDark">
                         We are dedicated to educating future physicians and empowering them to drive innovation in medicine. Our curriculum provides extensive clinical practice opportunities to help you become an exceptional physician.
                     </p>
                     <div className="grid gap-3 my-6">
-                        <span className="flex items-start gap-2 text-xl font-medium">
-                            <FaCheckDouble className="text-2xl text-themelight" />
+                        <span className="flex items-start gap-2 sm:text-xl text-lg font-medium">
+                            <FaCheckDouble className="sm:text-2xl text-lg text-themelight" />
                             Clinical Rotations available in USA, India, UK, Canada, Guyana
                         </span>
-                        <span className="flex items-start gap-2 text-xl font-medium">
-                            <FaCheckDouble className="text-2xl text-themelight" />
+                        <span className="flex items-start gap-2 sm:text-xl text-lg font-medium">
+                            <FaCheckDouble className="sm:text-2xl text-lg text-themelight" />
                             3 Admission Cycles: January, May, September
                         </span>
-                        <span className="flex items-start gap-2 text-xl font-medium">
-                            <FaCheckDouble className="text-2xl text-themelight" />
+                        <span className="flex items-start gap-2 sm:text-xl text-lg font-medium">
+                            <FaCheckDouble className="sm:text-2xl text-lg text-themelight" />
                             State-of-the-Art Campus in Guyana with Full Wireless Access
                         </span>
                     </div>
                     <div className="grid grid-cols-3 mt-10">
                         <div className="flex flex-col items-center gap-2 text-center text-balance" ref={counter01Ref}>
-                            <span className="text-6xl text-themeDark font-bold">
-                            {!counter01InView ? "0" : <CountUp end={100} className="text-6xl" duration={2} />}+
+                            <span className="md:text-6xl sm:text-5xl text-3xl text-themeDark font-bold">
+                            {!counter01InView ? "0" : <CountUp end={100} className="md:text-6xl sm:text-5xl text-3xl" duration={2} />}+
                             </span>
-                            <span className="opacity-70">Graduates practicing in USA, India, UK, Guyana</span>
+                            <span className="opacity-70 max-sm:text-sm">Graduates practicing in USA, India, UK, Guyana</span>
                         </div>
                         <div className="flex flex-col items-center gap-2 text-center text-balance" ref={counter02Ref}>
-                            <span className="text-6xl text-themeDark font-bold">
-                            {!counter02InView ? "0" : <CountUp end={85} className="text-6xl" duration={2} />}%
+                            <span className="md:text-6xl sm:text-5xl text-3xl text-themeDark font-bold">
+                            {!counter02InView ? "0" : <CountUp end={85} className="md:text-6xl sm:text-5xl text-3xl" duration={2} />}%
                             </span>
-                            <span className="opacity-70">USMLE Pass Rate</span>
+                            <span className="opacity-70 max-sm:text-sm">USMLE Pass Rate</span>
                         </div>
                         <div className="flex flex-col items-center gap-2 text-center text-balance" ref={counter03Ref}>
-                            <span className="text-6xl text-themeDark font-bold">
-                            {!counter03InView ? "0" : <CountUp end={95} className="text-6xl" duration={2} />}%
+                            <span className="md:text-6xl sm:text-5xl text-3xl text-themeDark font-bold">
+                            {!counter03InView ? "0" : <CountUp end={95} className="md:text-6xl sm:text-5xl text-3xl" duration={2} />}%
                             </span>
-                            <span className="opacity-70">PLAB Pass Rate</span>
+                            <span className="opacity-70 max-sm:text-sm">PLAB Pass Rate</span>
                         </div>
                     </div>
                 </div>

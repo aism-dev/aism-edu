0152e6083e44e478880c5a554328e4479ae740f5
fabiconcept <favourajokubi@gmail.com>
2024-10-23 09:10:33 +0100

    Update MiniInfoTwo component: adjust grid layout, padding, and font size

--- app/home-page components/MiniInfoTwo.tsx
+++ app/home-page components/MiniInfoTwo.tsx
@@ -2,7 +2,7 @@ import BubbleText from "../general components/BubbleText";
 import Button from "../general components/Button";
 export default function MiniInfoTwo() {
     return (
-        <div className="grid gap-14 sm:grid-cols-[auto_1fr_auto] w-full px-20 py-10 items-center bg-black/5">
+        <div className="grid sm:gap-14 gap-7 sm:grid-cols-[auto_1fr_auto] w-full sm:px-20 px-10 py-10 items-center bg-black/5">
             <div className="w-fit">
                 <Button sizeVariation="XL">
                     Join Us
@@ -12,7 +12,7 @@ export default function MiniInfoTwo() {
                 <span>Join AISM and unlock your potential with world-class faculty, cutting-edge facilities, and a diverse, supportive community. Gain the expertise and confidence to excel in medicine and make a meaningful impact on global healthcare. Your journey starts here!</span>
             </p>
             <div>
-                <BubbleText formatClass="text-5xl text-theme" text="Shape Your Future" />
+                <BubbleText formatClass="sm:text-5xl text-4xl text-theme" text="Shape Your Future" />
             </div>
         </div>
     )

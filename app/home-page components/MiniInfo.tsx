9126a6864d8da1fe9f58c5219cc4c417947eda8e
fabiconcept <favourajokubi@gmail.com>
2024-10-23 08:46:13 +0100

    Implement Responsiveness in MiniInfo Section on the HomePage

--- app/home-page components/MiniInfo.tsx
+++ app/home-page components/MiniInfo.tsx
@@ -1,11 +1,11 @@
 import BubbleText from "../general components/BubbleText";
 export default function MiniInfo() {
     return (
-        <div className="grid grid-cols-2 w-full px-40 pb-10 items-center gap-5">
+        <div className="grid md:grid-cols-2 w-full md:px-40 px-10 pb-10 items-center gap-5 max-sm:text-theme">
             <div>
-                <BubbleText formatClass="text-5xl" text="Turning your Dreams into Reality" />
+                <BubbleText formatClass="sm:text-5xl text-4xl" text="Turning your Dreams into Reality" />
             </div>
-            <p className="max-w-[clamp(30rem,40%,20rem)] justify-self-end grid gap-4">
+            <p className="max-w-[clamp(30rem,40%,20rem)] md:justify-self-end grid gap-4">
                 <span>The American International School of Medicine began in 1999 and has graduated hundreds of medical doctors who are in specialty clinical training and practicing medicine in the USA, Guyana, UK, Pakistan, Caribbean, and other countries worldwide.</span>
                 <span>We focus on training physicians who can excel in patient-care research and education as a part of their professional development.</span>
             </p>

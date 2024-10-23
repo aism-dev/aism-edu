2757f3629edadede0e9e628e1d67faa7264d3c12
fabiconcept <favourajokubi@gmail.com>
2024-10-23 20:33:15 +0100

    Implement Responsiveness in WhatWeCanDoForYou Section on the AboutPage

--- app/(pages)/about/Components/WhatWeCanDoForYou.tsx
+++ app/(pages)/about/Components/WhatWeCanDoForYou.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 export default function WhatWeCanDoForYou() {
     return (
         <>
-            <div className="grid grid-cols-2 w-full px-40 pb-10 items-center gap-5" id="TopOfPage">
+            <div className="grid md:grid-cols-2 w-full md:px-40 sm:px-10 px-8 pb-10 items-center gap-5" id="TopOfPage">
                 <div>
-                    <BubbleText formatClass="text-5xl" text="What AISM can do for you?" />
+                    <BubbleText formatClass="sm:text-5xl text-4xl" text="What AISM can do for you?" />
                 </div>
-                <p className="max-w-[clamp(35rem,40%,20rem)] justify-self-end grid gap-4">
+                <p className="sm:max-w-[clamp(35rem,40%,20rem)] w-full justify-self-end grid gap-4">
                     <span className="text-lg">The American International School of Medicine (AISM), established in 1999, has proudly graduated hundreds of medical doctors now practicing and specializing in countries like the USA, Guyana, the UK, Pakistan, and across the Caribbean. Our mission is to train physicians who excel in patient care, research, and education, ensuring a well-rounded professional development experience.</span>
                     <span className="opacity-70">AISM has campuses in Atlanta, Georgia, Georgetown, Guyana, the UK, and Nigeria. Our students gain invaluable hands-on experience through clinical rotations at hospitals in the United States, Canada, the UK, and Guyana, with opportunities for elective rotations in other countries.</span>
                 </p>

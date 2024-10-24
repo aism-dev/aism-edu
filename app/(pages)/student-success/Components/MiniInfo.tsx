7f979eca0f4e67dad452e30b1850d3c5a4e670ad
fabiconcept <favourajokubi@gmail.com>
2024-10-24 09:32:21 +0100

    Implement Responsiveness in MiniInfo Section on the StudentSuccessPage

--- app/(pages)/student-success/Components/MiniInfo.tsx
+++ app/(pages)/student-success/Components/MiniInfo.tsx
@@ -2,11 +2,11 @@ import BubbleText from "@/app/general components/BubbleText";
 
 export default function MiniInfo() {
     return (
-        <div className="grid grid-cols-2 w-full px-40 py-24 items-center gap-5 border-b shadow-lg shadow-black/5 bg-theme">
+        <div className="grid sm:grid-cols-2 w-full md:px-40 sm:px-20 px-10 py-24 items-center gap-5 border-b shadow-lg shadow-black/5 bg-theme">
             <div>
-                <BubbleText formatClass="text-5xl text-white" text="Student Success Stories" />
+                <BubbleText formatClass="sm:text-5xl text-4xl text-white" text="Student Success Stories" />
             </div>
-            <p className="max-w-[clamp(30rem,40%,20rem)] justify-self-end grid gap-4">
+            <p className="sm:max-w-[clamp(30rem,40%,20rem)] w-full sm:justify-self-end grid gap-4">
                 <span className="text-white">AISM students excel in academics and beyond, securing top residencies and making global impacts. Explore their stories and see how AISM has shaped their medical journeys.</span>
             </p>
         </div>

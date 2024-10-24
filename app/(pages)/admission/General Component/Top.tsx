28938d4794fa51e4212497488e9bbfe508c64678
fabiconcept <favourajokubi@gmail.com>
2024-10-24 15:20:02 +0100

    Add AnimatedEntrance component to MiniInfo function in Top.tsx

--- app/(pages)/admission/General Component/Top.tsx
+++ app/(pages)/admission/General Component/Top.tsx
@@ -1,3 +1,4 @@
+import { AnimatedEntrance } from "@/app/general components/AnimatedEntrance";
 import BubbleText from "@/app/general components/BubbleText";
 
 export default function MiniInfo({
@@ -5,13 +6,13 @@ export default function MiniInfo({
     p
 }: { bubbleText: string, p: string }) {
     return (
-        <div className="grid sm:grid-cols-2 w-full md:px-40 sm:px-20 px-10 py-24 items-center gap-5 border-b shadow-lg shadow-black/5 bg-theme">
+        <AnimatedEntrance direction="right" className="grid sm:grid-cols-2 w-full md:px-40 sm:px-20 px-10 py-24 items-center gap-5 border-b shadow-lg shadow-black/5 bg-theme">
             <div>
                 <BubbleText formatClass="sm:text-5xl text-4xl text-white" text={bubbleText} />
             </div>
             <p className="sm:max-w-[clamp(30rem,40%,20rem)] w-full justify-self-end grid gap-4">
                 <span className="text-white">{p}</span>
             </p>
-        </div>
+        </AnimatedEntrance>
     )
 }

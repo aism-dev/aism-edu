4397830a90989d7ed61bcdb0cd4a80d5b740a2b7
fabiconcept <favourajokubi@gmail.com>
2024-10-24 16:04:51 +0100

    Add AnimatedEntrance component to MapViewSection and TopSection components.

--- app/(pages)/contact-us/Components/MapViewSection.tsx
+++ app/(pages)/contact-us/Components/MapViewSection.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import { myAPi } from '@/lib/Variables/useful';
+import { AnimatedEntrance } from '@/app/general components/AnimatedEntrance';
 
 const customIcon = L.icon({
     iconUrl: "https://aism-edu.sirv.com/icons/AISM-thick.png",
@@ -17,17 +18,19 @@ export default function MapViewSection() {
     const position: LatLngExpression = [33.826806, -84.118446];
 
     return (
-        <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }} scrollWheelZoom={false} className='z-0'>
-            <TileLayer
-                url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${myAPi}`}
-                attribution='&copy; <a href="https://fabiconcept.online">Fabiconcept</a>'
-                id="mapbox/streets-v11" // You can choose different map styles like 'streets-v11', 'satellite-v9', etc.
-                tileSize={512}
-                zoomOffset={-1}
-            />
-            <Marker icon={customIcon} position={position}>
-                <Popup>We are located here</Popup>
-            </Marker>
-        </MapContainer>
+        <AnimatedEntrance>
+            <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }} scrollWheelZoom={false} className='z-0'>
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${myAPi}`}
+                    attribution='&copy; <a href="https://fabiconcept.online">Fabiconcept</a>'
+                    id="mapbox/streets-v11" // You can choose different map styles like 'streets-v11', 'satellite-v9', etc.
+                    tileSize={512}
+                    zoomOffset={-1}
+                />
+                <Marker icon={customIcon} position={position}>
+                    <Popup>We are located here</Popup>
+                </Marker>
+            </MapContainer>
+        </AnimatedEntrance>
     )
 }
--- app/(pages)/contact-us/Components/TopSection.tsx
+++ app/(pages)/contact-us/Components/TopSection.tsx
@@ -1,30 +1,31 @@
+import { AnimatedEntrance } from "@/app/general components/AnimatedEntrance";
 import Link from "next/link";
 import { FaLocationDot, FaPhone } from "react-icons/fa6";
 
 export default function TopSection() {
     return (
         <div className="md:px-20 sm:px-10 px-5 py-20 grid sm:grid-cols-2 sm:gap-5 gap-8">
-            <div className="">
+            <AnimatedEntrance className="">
                 <h1 className="md:text-5xl sm:text-4xl text-3xl text-theme font-medium">Get in Touch</h1>
                 <p className="sm:max-w-[80%] text-balance mt-3">We&apos;d love to hear from you! Whether you have questions, need support, or want to learn more about our services, our team is here to help.</p>
-            </div>
+            </AnimatedEntrance>
             <div className="grid grid-cols-2">
-                <div className="grid gap-2">
+                <AnimatedEntrance className="grid gap-2">
                     <div className="sm:h-9 h-8 sm:w-9 w-8 rounded-lg bg-theme/10 text-theme grid place-items-center"><FaLocationDot /></div>
                     <h5 className="mt-2 sm:text-xl text-lg font-medium text-theme">Our Address</h5>
                     <Link href={"https://maps.app.goo.gl/HSDcmkkJGtjU198d8"} target="_blank" className="grid w-fit">
                         <span>1755 E. Park Place, Blvd.</span> 
                         <span>Stone Mountain, Ga.  30087</span>
                     </Link>
-                </div>
-                <div className="grid gap-2">
+                </AnimatedEntrance>
+                <AnimatedEntrance className="grid gap-2">
                     <div className="sm:h-9 h-8 sm:w-9 w-8 rounded-lg bg-theme/10 text-theme grid place-items-center"><FaPhone /></div>
                     <h5 className="mt-2 sm:text-xl text-lg font-medium text-theme">Our Contact Info.</h5>
                     <p className="grid">
                         <Link className="w-fit font-medium" href={"tel:+14047566737"}>(404) 756 - 6737</Link> 
                         <Link className="w-fit font-medium" href={"mailto:information@aism.edu"}>information@aism.edu</Link>
                     </p>
-                </div>
+                </AnimatedEntrance>
             </div>
         </div>
     )

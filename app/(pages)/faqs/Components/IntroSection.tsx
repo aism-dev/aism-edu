1e8958a8e8cf1fefdd460666707ec4a15baeeb4f
fabiconcept <favourajokubi@gmail.com>
2024-10-24 09:40:11 +0100

    Implement Responsiveness in IntroSection Section on the FaqsPage

--- app/(pages)/faqs/Components/IntroSection.tsx
+++ app/(pages)/faqs/Components/IntroSection.tsx
@@ -2,11 +2,11 @@ import BubbleText from "@/app/general components/BubbleText";
 
 export default function IntroSection() {
     return (
-        <div className="grid grid-cols-2 w-full px-40 items-center gap-5 border-b shadow-lg shadow-black/5 bg-theme py-24">
+        <div className="grid sm:grid-cols-2 w-full md:px-40 sm:px-10 px-8 items-center gap-5 border-b shadow-lg shadow-black/5 bg-theme py-24">
             <div>
-                <BubbleText formatClass="text-5xl text-white" text="Frequently Asked Question?" />
+                <BubbleText formatClass="sm:text-5xl text-4xl text-white" text="Frequently Asked Question?" />
             </div>
-            <p className="max-w-[clamp(30rem,40%,20rem)] justify-self-end grid gap-4">
+            <p className="sm:max-w-[clamp(30rem,40%,20rem)] w-full sm:justify-self-end grid gap-4">
                 <span className="text-white">Find answers to common questions about our programs, admissions, and campus life at AISM. If you don&apos;t see your question here, rest assured—we&apos;re here to help!</span>
             </p>
         </div>

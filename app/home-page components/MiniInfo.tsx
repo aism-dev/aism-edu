cbd3f556d2bd1649b217363b2e3c4a3e5a904264
fabiconcept <favourajokubi@gmail.com>
2024-10-24 15:40:37 +0100

    Wrap MiniInfo component with AnimatedEntrance

--- app/home-page components/MiniInfo.tsx
+++ app/home-page components/MiniInfo.tsx
@@ -1,7 +1,8 @@
+import { AnimatedEntrance } from "../general components/AnimatedEntrance";
 import BubbleText from "../general components/BubbleText";
 export default function MiniInfo() {
     return (
-        <div className="grid md:grid-cols-2 w-full md:px-40 px-10 pb-10 items-center gap-5 max-sm:text-theme">
+        <AnimatedEntrance className="grid md:grid-cols-2 w-full md:px-40 px-10 pb-10 items-center gap-5 max-sm:text-theme">
             <div>
                 <BubbleText formatClass="sm:text-5xl text-4xl" text="Turning your Dreams into Reality" />
             </div>
@@ -9,6 +10,6 @@ export default function MiniInfo() {
                 <span>The American International School of Medicine began in 1999 and has graduated hundreds of medical doctors who are in specialty clinical training and practicing medicine in the USA, Guyana, UK, Pakistan, Caribbean, and other countries worldwide.</span>
                 <span>We focus on training physicians who can excel in patient-care research and education as a part of their professional development.</span>
             </p>
-        </div>
+        </AnimatedEntrance>
     )
 }

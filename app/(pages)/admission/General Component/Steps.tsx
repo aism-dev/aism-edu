463ceae49a0a705830edf02308fac9a8ea3a2be5
fabiconcept <favourajokubi@gmail.com>
2024-10-24 09:00:38 +0100

    Add 'noStep' prop to Steps component and update tuition-fees/page.tsx to pass 'noStep' prop

--- app/(pages)/admission/General Component/Steps.tsx
+++ app/(pages)/admission/General Component/Steps.tsx
@@ -1,15 +1,15 @@
 import clsx from "clsx";
 import Step from "./Step";
 
-export default function Steps({data}: {data: any[]}) {
+export default function Steps({ data, noStep }: { data: any[], noStep?: boolean }) {
     return (
         <div className="grid sm:p-20 px-8 py-20 gap-5">
             {data.map((process, id) => (
                 <Step key={id} isOdd={(id + 1) % 2 !== 0}>
-                    <h2 className={clsx(
+                    {!noStep && <h2 className={clsx(
                         "sm:text-4xl text-2xl font-semibold flex items-center gap-2",
                         (id + 1) % 2 !== 0 ? "justify-end" : ""
-                    )}>Step {id + 1}</h2>
+                    )}>Step {id + 1}</h2>}
                     <h4 className="text-xl text-theme my-2 font-semibold">{process.title}</h4>
                     {process.content}
                 </Step>
--- app/(pages)/admission/tuition-fees/page.tsx
+++ app/(pages)/admission/tuition-fees/page.tsx
@@ -11,6 +11,7 @@ export default function page() {
             />
             <Steps 
                 data={tuitionSteps}
+                noStep
             />
         </div>
     )

8f0c26c6379e954811497db1ad2d1d25fb752197
fabiconcept <favourajokubi@gmail.com>
2024-10-24 09:41:49 +0100

    Implement Responsiveness in FaQsSection Section on the FaqsPage

--- app/(pages)/faqs/Components/FaQsSection.tsx
+++ app/(pages)/faqs/Components/FaQsSection.tsx
@@ -3,8 +3,8 @@ import FaQs from "./FaQs";
 
 export default function FaQsSection() {
     return (
-        <div className="p-20">
-            <div className="grid grid-cols-2 gap-10">
+        <div className="py-20 md:px-20 sm:px-10 px-5">
+            <div className="grid sm:grid-cols-2 gap-10">
                 <div className="flex flex-col gap-6">
                     {faqData.map((faq, idx)=>(
                         idx % 2 === 1 && <FaQs key={idx} {...faq} />

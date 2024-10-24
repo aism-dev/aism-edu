909f143e5ebf2e90c636d6245f5db43f29444fb1
fabiconcept <favourajokubi@gmail.com>
2024-10-24 09:07:13 +0100

    Implement Responsiveness in BodySection Section on the AcademicPages

--- app/(pages)/acedemic-programs/Components/Body.tsx
+++ app/(pages)/acedemic-programs/Components/Body.tsx
@@ -4,7 +4,7 @@ import SideComponnet from "./SideComponnet";
 
 export default function BodySection(mainProps: MainComponentProps) {
     return (
-        <div className="py-10 px-20 grid md:grid-cols-[auto_25rem] gap-8 grid-cols-1">
+        <div className="py-10 md:px-20 sm:px-10 px-5 grid md:grid-cols-[auto_25rem] gap-8 grid-cols-1">
             <MainComponent
                 {...mainProps}
              />

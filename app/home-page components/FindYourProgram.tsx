df9edc91c19e3bf53bd7443788e7bd7124131d03
fabiconcept <favourajokubi@gmail.com>
2024-10-24 15:54:32 +0100

    Add AnimatedEntrance component to FindYourProgram and MiniInfoTwo components

--- app/home-page components/FindYourProgram.tsx
+++ app/home-page components/FindYourProgram.tsx
@@ -6,6 +6,7 @@ import { motion } from  "framer-motion";
 import Image from "next/image";
 import Button from "../general components/Button";
 import { useIntersection } from "react-use";
+import { AnimatedEntrance } from "../general components/AnimatedEntrance";
 
 export default function FindYourProgram() {
     const [currentItem, setCurrentItem] = useState(0);
@@ -49,61 +50,63 @@ export default function FindYourProgram() {
 
     return (
         <div className="py-20 max-sm:px-10" ref={containerRef}>
-            <div className="text-center flex flex-col items-center gap-4">
+            <AnimatedEntrance className="text-center flex flex-col items-center gap-4">
                 <h2 className="text-4xl font-medium text-theme">Find your Course</h2>
                 <p className="max-w-[40rem] leading-6 text-black/70">
                     Explore a range of programs tailored to your career goals. Choose the path that best suits your aspirations and start your journey in healthcare today.
                 </p>
-            </div>
+            </AnimatedEntrance>
             <div className="flex max-sm:flex-col gap-3 mt-8 px-4 max-sm:h-screen max-sm:min-h-[50rem]">
                 {Programs.map((program)=>(
-                    <div key={program.id} onClick={() => handleClick(program.id - 1)} className={clsx(
-                        "sm:h-[25rem] max-sm:min-h-[5rem] max-sm:mh min-w-40 transition-[width_1s_ease] duration-500-alt relative overflow-hidden",
+                    <AnimatedEntrance key={program.id} className={clsx(
                         currentItem === (program.id - 1) ? "flex-1" : "cursor-pointer"
                     )}>
-                        <div className={clsx(
-                            "h-full pointer-events-none select-none absolute top-0 left-0 -z-10",
-                            currentItem === (program.id - 1) ? "withGradient" : "withoutGradient"
+                        <div onClick={() => handleClick(program.id - 1)} className={clsx(
+                            "sm:h-[25rem] max-sm:min-h-[5rem] max-sm:mh min-w-40 transition-[width_1s_ease] duration-500-alt relative overflow-hidden",
                         )}>
-                            <Image
-                                src={program.img}
-                                alt={program.title}
-                                height={10800}
-                                width={7200}
-                                priority
-                                className="h-full min-w-full object-top object-cover self-start"
-                                />
-                        </div>
-                        <div className="absolute z-10 bottom-10 sm:left-24 left-10 text-white grid gap-3">
-                            <h3 className={clsx(
-                                "origin-bottom-left text-2xl whitespace-nowrap flex flex-col",
-                                currentItem === (program.id - 1) ? "font-semibold" : "sm:-rotate-90 translate-y-10 translate-x-16"
+                            <div className={clsx(
+                                "h-full pointer-events-none select-none absolute top-0 left-0 -z-10",
+                                currentItem === (program.id - 1) ? "withGradient" : "withoutGradient"
                             )}>
-                                {currentItem === (program.id - 1) && program.isComingSoon && <span className="text-gray-300/80 capitalize font-thin">coming soon</span>}
-                                {program.title} 
-                            </h3>
-                            <motion.p
-                                className="max-w-[80%] overflow-hidden opacity-80"
-                                initial={{ height: 0, opacity: 0 }}
-                                animate={currentItem === (program.id - 1) ? { height: "auto", opacity: 1 } : { height: 0, opacity: 0 }} 
-                                transition={currentItem === (program.id - 1) ? { duration: 0.5 } : { duration: 0.1}}
-                                exit={{ height: 0 }}
-
-                            >
-                                {program.description}
-                            </motion.p>
-                            <motion.div
-                                initial={{ height: 0, opacity: 0 }}
-                                animate={currentItem === (program.id - 1) ? { height: "auto", opacity: 1 } : { height: 0, opacity: 0 }} 
-                                transition={currentItem === (program.id - 1) ? { duration: 0.5 } : { duration: 0.1}}
-                                exit={{ height: 0 }}
-                            >
-                                <Button href={program.url} colorVariation="dark">
-                                    Explore {program.title}
-                                </Button>
-                            </motion.div>
+                                <Image
+                                    src={program.img}
+                                    alt={program.title}
+                                    height={10800}
+                                    width={7200}
+                                    priority
+                                    className="h-full min-w-full object-top object-cover self-start"
+                                    />
+                            </div>
+                            <div className="absolute z-10 bottom-10 sm:left-24 left-10 text-white grid gap-3">
+                                <h3 className={clsx(
+                                    "origin-bottom-left text-2xl whitespace-nowrap flex flex-col",
+                                    currentItem === (program.id - 1) ? "font-semibold" : "sm:-rotate-90 translate-y-10 max-sm:translate-x-16"
+                                )}>
+                                    {currentItem === (program.id - 1) && program.isComingSoon && <span className="text-gray-300/80 capitalize font-thin">coming soon</span>}
+                                    {program.title}
+                                </h3>
+                                <motion.p
+                                    className="max-w-[80%] overflow-hidden opacity-80"
+                                    initial={{ height: 0, opacity: 0 }}
+                                    animate={currentItem === (program.id - 1) ? { height: "auto", opacity: 1 } : { height: 0, opacity: 0 }}
+                                    transition={currentItem === (program.id - 1) ? { duration: 0.5 } : { duration: 0.1}}
+                                    exit={{ height: 0 }}
+                                >
+                                    {program.description}
+                                </motion.p>
+                                <motion.div
+                                    initial={{ height: 0, opacity: 0 }}
+                                    animate={currentItem === (program.id - 1) ? { height: "auto", opacity: 1 } : { height: 0, opacity: 0 }}
+                                    transition={currentItem === (program.id - 1) ? { duration: 0.5 } : { duration: 0.1}}
+                                    exit={{ height: 0 }}
+                                >
+                                    <Button href={program.url} colorVariation="dark">
+                                        Explore {program.title}
+                                    </Button>
+                                </motion.div>
+                            </div>
                         </div>
-                    </div>
+                    </AnimatedEntrance>
                 ))}
             </div>
         </div>
--- app/home-page components/MiniInfoTwo.tsx
+++ app/home-page components/MiniInfoTwo.tsx
@@ -1,8 +1,9 @@
+import { AnimatedEntrance } from "../general components/AnimatedEntrance";
 import BubbleText from "../general components/BubbleText";
 import Button from "../general components/Button";
 export default function MiniInfoTwo() {
     return (
-        <div className="grid sm:gap-14 gap-7 sm:grid-cols-[auto_1fr_auto] w-full sm:px-20 px-10 py-10 items-center bg-black/5">
+        <AnimatedEntrance className="grid sm:gap-14 gap-7 sm:grid-cols-[auto_1fr_auto] w-full sm:px-20 px-10 py-10 items-center bg-black/5">
             <div className="w-fit">
                 <Button sizeVariation="XL">
                     Join Us
@@ -14,6 +15,6 @@ export default function MiniInfoTwo() {
             <div>
                 <BubbleText formatClass="sm:text-5xl text-4xl text-theme" text="Shape Your Future" />
             </div>
-        </div>
+        </AnimatedEntrance>
     )
 }

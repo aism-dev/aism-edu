15a28767d570cef290a4fbccac14c2155fda9a4d
fabiconcept <favourajokubi@gmail.com>
2024-10-24 15:11:23 +0100

    Wrap 'Related Academic Programs' and 'Questions About Applying' sections with AnimatedEntrance component

--- app/(pages)/acedemic-programs/Components/SideComponnet.tsx
+++ app/(pages)/acedemic-programs/Components/SideComponnet.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { FaAngleRight, FaAt, FaPhoneFlip } from "react-icons/fa6";
 import { useEffect, useState } from "react";
+import { AnimatedEntrance } from "@/app/general components/AnimatedEntrance";
 
 const programs = [
     { id: 1, name: "Pre-Health (Pre-Medical) Sciences", link: "/acedemic-programs/pre-med-course" },
@@ -21,50 +22,53 @@ export default function SideComponent() {
 
     return (
         <div className="md:block hidden">
-            <div className="p-5 bg-theme text-white text-xl font-semibold">
-                Related Academic Programs
-            </div>
-            {pathname && (
+            <AnimatedEntrance>
+                <div className="p-5 bg-theme text-white text-xl font-semibold">
+                    Related Academic Programs
+                </div>
+                {pathname && (
+                    <div className="grid">
+                        {programs.filter((program) => program.link !== pathname).map((program) => (
+                            <div key={program.id} className="p-4 border">
+                                <Link href={program.link} className="flex justify-between group no-underline">
+                                    {program.name}
+                                    <div className="aspect-square bg-theme text-white group-hover:opacity-100 opacity-0 -translate-x-5 group-hover:translate-x-0 h-7 grid place-items-center">
+                                        <FaAngleRight />
+                                    </div>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </AnimatedEntrance>
+
+            <AnimatedEntrance>
+                <div className="p-5 bg-theme text-white text-xl font-semibold mt-10">
+                    Questions About Applying
+                </div>
                 <div className="grid">
-                    {programs.filter((program) => program.link !== pathname).map((program) => (
-                        <div key={program.id} className="p-4 border">
-                            <Link href={program.link} className="flex justify-between group no-underline">
-                                {program.name}
-                                <div className="aspect-square bg-theme text-white group-hover:opacity-100 opacity-0 -translate-x-5 group-hover:translate-x-0 h-7 grid place-items-center">
-                                    <FaAngleRight />
-                                </div>
-                            </Link>
+                    <div className="p-4 border">
+                        <div className="flex justify-between group no-underline">
+                            <span>E-Mail Inquiry : <Link href={"mailto:admissions@aism.edu"}>admissions@aism.edu</Link></span>
+                            <div className="aspect-square text-theme group-hover:opacity-100 opacity-0 -translate-x-5 group-hover:translate-x-0 h-7 grid place-items-center">
+                                <Link href={"mailto:admissions@aism.edu"} className="no-underline">
+                                    <FaAt />
+                                </Link>
+                            </div>
                         </div>
-                    ))}
-                </div>
-            )}
-
-            <div className="p-5 bg-theme text-white text-xl font-semibold mt-10">
-                Questions About Applying
-            </div>
-
-            <div className="grid">
-                <div className="p-4 border">
-                    <div className="flex justify-between group no-underline">
-                        <span>E-Mail Inquiry : <Link href={"mailto:admissions@aism.edu"}>admissions@aism.edu</Link></span>
-                        <div className="aspect-square text-theme group-hover:opacity-100 opacity-0 -translate-x-5 group-hover:translate-x-0 h-7 grid place-items-center">
-                            <Link href={"mailto:admissions@aism.edu"} className="no-underline">
-                                <FaAt />
-                            </Link>
+                    </div>
+                    <div className="p-4 border">
+                        <div className="flex justify-between group no-underline">
+                            <span>Phone : <Link href={"tel:+18664659966"}>+1 (866) 465 9966</Link></span>
+                            <div className="aspect-square text-theme group-hover:opacity-100 opacity-0 -translate-x-5 group-hover:translate-x-0 h-7 grid place-items-center">
+                                <Link href={"tel:+18664659966"} className="no-underline">
+                                    <FaPhoneFlip />
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="p-4 border">
-                    <div className="flex justify-between group no-underline">
-                        <span>Phone : <Link href={"tel:+18664659966"}>+1 (866) 465 9966</Link></span>
-                        <div className="aspect-square text-theme group-hover:opacity-100 opacity-0 -translate-x-5 group-hover:translate-x-0 h-7 grid place-items-center">
-                            <Link href={"tel:+18664659966"} className="no-underline">
-                                <FaPhoneFlip />
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </AnimatedEntrance>
         </div>
     )
 }

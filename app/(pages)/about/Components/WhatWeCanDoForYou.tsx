e6b2f018c9073d28898e3624730dc16457c54388
fabiconcept <favourajokubi@gmail.com>
2024-10-24 15:27:44 +0100

    Wrap two div elements with AnimatedEntrance component in WhatWeCanDoForYou.tsx

--- app/(pages)/about/Components/WhatWeCanDoForYou.tsx
+++ app/(pages)/about/Components/WhatWeCanDoForYou.tsx
@@ -1,10 +1,11 @@
+import { AnimatedEntrance } from "@/app/general components/AnimatedEntrance";
 import BubbleText from "@/app/general components/BubbleText";
 import Image from "next/image";
 
 export default function WhatWeCanDoForYou() {
     return (
         <>
-            <div className="grid md:grid-cols-2 w-full md:px-40 sm:px-10 px-8 pb-10 items-center gap-5" id="TopOfPage">
+            <AnimatedEntrance className="grid md:grid-cols-2 w-full md:px-40 sm:px-10 px-8 pb-10 items-center gap-5" id="TopOfPage">
                 <div>
                     <BubbleText formatClass="sm:text-5xl text-4xl" text="What AISM can do for you?" />
                 </div>
@@ -12,8 +13,8 @@ export default function WhatWeCanDoForYou() {
                     <span className="text-lg">The American International School of Medicine (AISM), established in 1999, has proudly graduated hundreds of medical doctors now practicing and specializing in countries like the USA, Guyana, the UK, Pakistan, and across the Caribbean. Our mission is to train physicians who excel in patient care, research, and education, ensuring a well-rounded professional development experience.</span>
                     <span className="opacity-70">AISM has campuses in Atlanta, Georgia, Georgetown, Guyana, the UK, and Nigeria. Our students gain invaluable hands-on experience through clinical rotations at hospitals in the United States, Canada, the UK, and Guyana, with opportunities for elective rotations in other countries.</span>
                 </p>
-            </div>
-            <div className="h-96 overflow-y-hidden grid place-items-center">
+            </AnimatedEntrance>
+            <AnimatedEntrance className="h-96 overflow-y-hidden grid place-items-center">
                 <Image
                     src={"https://aism-edu.sirv.com/301617876_512872740529074_7075262364263306893_n_LE_auto_x2.jpg"}
                     alt="AISM Group Photo"
@@ -21,7 +22,7 @@ export default function WhatWeCanDoForYou() {
                     height={1208}
                     className="object-contain object-center w-full h-auto md:-mt-24 pointer-events-none select-none"
                 />
-            </div>
+            </AnimatedEntrance>
         </>
     )
 }

7b50ec2d137f5c55a95446f35903d781634565f1
fabiconcept <favourajokubi@gmail.com>
2024-10-29 09:41:06 +0100

    Add FormData interface and initialize formData state in FormBody component; update FormBodyContext to include formData and setFormData

--- app/(pages)/acedemic-programs/application-form/[program]/Components/FormBody.tsx
+++ app/(pages)/acedemic-programs/application-form/[program]/Components/FormBody.tsx
@@ -2,21 +2,35 @@
 import React, { useState } from "react";
 import Form from "./Form";
 
+interface FormData {
+    fullName: string,
+    email: string,
+    phone: string,
+    address: string,
+    nationality: string,
+    gender: string,
+    birthDate: string,
+    highSchool: string,
+    highSchoolLocation: string,
+    highSchoolGraduation: string,
+    university: string,
+    major: string,
+    intendedProgram: string,
+    preferredStartDate: string,
+    isTransferStudent: boolean,
+}
+
 interface FormBodyContext {
     currentTab: number,
     setCurrentTab: React.Dispatch<React.SetStateAction<number>>,
+    formData: FormData, 
+    setFormData: React.Dispatch<React.SetStateAction<FormData>>
 }
 
 const formBodyContext: FormBodyContext = {
     currentTab: 1,
-    setCurrentTab: ()=>{}
-}
-
-export const FormBodyContext = React.createContext(formBodyContext);
-
-export default function FormBody() {
-    const [currentTab,  setCurrentTab] = useState(1);
-    const [] = useState({
+    setCurrentTab: ()=>{},
+    formData: {
         fullName: "",
         email: "",
         phone: "",
@@ -31,11 +45,35 @@ export default function FormBody() {
         major: "",
         intendedProgram: "",
         preferredStartDate: "",
-        isTransferStudent: "",
+        isTransferStudent: false,
+    },
+    setFormData: ()=>{}
+}
+
+export const FormBodyContext = React.createContext(formBodyContext);
+
+export default function FormBody() {
+    const [currentTab,  setCurrentTab] = useState(1);
+    const [formData, setFormData] = useState({
+        fullName: "",
+        email: "",
+        phone: "",
+        address: "",
+        nationality: "",
+        gender: "",
+        birthDate: "",
+        highSchool: "",
+        highSchoolLocation: "",
+        highSchoolGraduation: "",
+        university: "",
+        major: "",
+        intendedProgram: "",
+        preferredStartDate: "",
+        isTransferStudent: false,
     })
 
     return (
-        <FormBodyContext.Provider value={{ currentTab, setCurrentTab }}>
+        <FormBodyContext.Provider value={{ currentTab, setCurrentTab, formData, setFormData }}>
             <div className="py-10 xl:px-20 md:px-10 sm:px-7 px-5">
                 <h1 className="text-3xl font-semibold text-theme">Apply to AISM &mdash; Start Your Journey</h1>
                 <p className="my-3">

f8ef46130c532f0a17b9378221e8849ec3db7ef2
fabiconcept <favourajokubi@gmail.com>
2024-10-31 10:11:04 +0100

    Replace Leaflet map with Google Maps iframe in MapViewSection component, and remove MapViewSectionGoogle component

--- app/(pages)/contact-us/Components/MapViewSection.tsx
+++ app/(pages)/contact-us/Components/MapViewSection.tsx
@@ -1,36 +1,13 @@
-"use client"
-import { LatLngExpression } from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
-import { myAPi } from '@/lib/Variables/useful';
-import { AnimatedEntrance } from '@/app/general components/AnimatedEntrance';
-
-const customIcon = L.icon({
-    iconUrl: "https://aism-edu.sirv.com/icons/AISM-thick.png",
-    iconSize: [38, 38],
-    iconAnchor: [22, 38],
-    popupAnchor: [0, -30],
-});
-
-export default function MapViewSection() {
-    const position: LatLngExpression = [33.826806, -84.118446];
-
+export default function MapViewSectionGoogle() {
     return (
-        <AnimatedEntrance>
-            <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }} scrollWheelZoom={false} className='z-0'>
-                <TileLayer
-                    url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${myAPi}`}
-                    attribution='&copy; <a href="https://fabiconcept.online">Fabiconcept</a>'
-                    id="mapbox/streets-v11" // You can choose different map styles like 'streets-v11', 'satellite-v9', etc.
-                    tileSize={512}
-                    zoomOffset={-1}
-                />
-                <Marker icon={customIcon} position={position}>
-                    <Popup>We are located here</Popup>
-                </Marker>
-            </MapContainer>
-        </AnimatedEntrance>
-    )
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3314.4476190430705!2d-84.11848909999999!3d33.8265653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5aff24a97c9c7%3A0x73b61a14e2227369!2s1755%20E%20Park%20Pl%20Blvd%2C%20Stone%20Mountain%2C%20GA%2030087%2C%20USA!5e0!3m2!1sen!2sng!4v1730365642929!5m2!1sen!2sng"
+            width="100%"
+            className="border-none h-[60dvh]"
+            allowFullScreen={false}
+            loading="lazy"
+            title="Find American International School of Medicine"
+            referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+    );
 }

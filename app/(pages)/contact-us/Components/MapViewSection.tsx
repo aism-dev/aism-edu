6181e93ea82207d66fe564e52ee39aae576899fc
fabiconcept <favourajokubi@gmail.com>
2024-11-11 13:00:34 +0100

    Update className attribute of iframe element in MapViewSectionGoogle component

--- app/(pages)/contact-us/Components/MapViewSection.tsx
+++ app/(pages)/contact-us/Components/MapViewSection.tsx
@@ -3,7 +3,7 @@ export default function MapViewSectionGoogle() {
         <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3314.4476190430705!2d-84.11848909999999!3d33.8265653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5aff24a97c9c7%3A0x73b61a14e2227369!2s1755%20E%20Park%20Pl%20Blvd%2C%20Stone%20Mountain%2C%20GA%2030087%2C%20USA!5e0!3m2!1sen!2sng!4v1730365642929!5m2!1sen!2sng"
             width="100%"
-            className="border-none h-[60dvh]"
+            className="border-none sm:h-[70dvh] h-[80dvh]"
             allowFullScreen={false}
             loading="lazy"
             title="Find American International School of Medicine"

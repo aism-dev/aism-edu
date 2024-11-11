30c4b60e38115f91bd52ea07494fb7fac7afe2b0
fabiconcept <favourajokubi@gmail.com>
2024-11-11 12:37:38 +0100

    Add metadata for Meet Our Administration page

--- app/(pages)/about/meet-our-administration/page.tsx
+++ app/(pages)/about/meet-our-administration/page.tsx
@@ -2,6 +2,21 @@ import { teamsItems } from "@/lib/Variables/Teams";
 import Team from "./Components/Team";
 import Top from "./Components/Top";
 
+import { Metadata } from "next";
+import { aismMetadata } from "@/lib/Metadata";
+
+export const metadata: Metadata = {
+    ...aismMetadata,
+    title: "AISM Leadership | Guiding Future Generations of Physicians",
+    description: "Get to know the dedicated leadership team at AISM, committed to fostering excellence in medical education and supporting your journey toward a successful healthcare career.",
+    openGraph: {
+        ...aismMetadata.openGraph,
+        title: "AISM Leadership | Guiding Future Generations of Physicians",
+        description: "Get to know the dedicated leadership team at AISM, committed to fostering excellence in medical education and supporting your journey toward a successful healthcare career.",
+        url: aismMetadata.openGraph?.url + "/about/meet-our-administration"
+    }
+};
+
 export default function MeetOurAdministration() {
     return (
         <div className="sm:mt-44 mt-36">

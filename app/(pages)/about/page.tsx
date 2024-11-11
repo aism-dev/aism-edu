db23c0d0c003b8de42a5db9a859262d28a3deb7c
fabiconcept <favourajokubi@gmail.com>
2024-11-11 12:29:18 +0100

    Add metadata to About page with title and description

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -4,6 +4,14 @@ import OurVision from "./Components/OurVision";
 import WhyChooseUs from "./Components/WhyChooseUs";
 import OurValues from "./Components/OurValues";
 import Affiliates from "./Components/Affiliates";
+import { aismMetadata } from "@/lib/Metadata";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    ...aismMetadata,
+    title: "About AISM | A Tradition of Excellence in Medical Education",
+    description: "Learn about AISM's guiding principles, our mission to transform medical education, and the values that drive our commitment to shaping compassionate, skilled healthcare professionals.",
+};
 
 export default function AboutPage() {
     return (

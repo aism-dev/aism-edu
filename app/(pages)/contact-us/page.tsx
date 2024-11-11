64552e704029b2e67871e7f832cffa09ff2b94f8
fabiconcept <favourajokubi@gmail.com>
2024-11-11 12:59:46 +0100

    Add metadata for Contact Us page

--- app/(pages)/contact-us/page.tsx
+++ app/(pages)/contact-us/page.tsx
@@ -1,5 +1,19 @@
 import MapViewSection from "./Components/MapViewSection";
 import TopSection from "./Components/TopSection";
+import { aismMetadata } from "@/lib/Metadata";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    ...aismMetadata,
+    title: "Contact AISM | Get in Touch with Our Admissions Team",
+    description: "Have questions about AISM? Reach out to our admissions team today. Contact us by phone, email, or through our online inquiry form. We’re here to assist you with your medical school journey.",
+    openGraph: {
+        ...aismMetadata.openGraph,
+        title: "Contact AISM | Get in Touch with Our Admissions Team",
+        description: "Have questions about AISM? Reach out to our admissions team today. Contact us by phone, email, or through our online inquiry form. We’re here to assist you with your medical school journey.",
+        url: aismMetadata.openGraph?.url + "/contact-us"
+    }
+};
 
 export default function ContactUsPage() {
     return (

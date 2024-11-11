83f3a71969c200e3ae469137a8d2d193c78a0f3b
fabiconcept <favourajokubi@gmail.com>
2024-11-11 12:58:29 +0100

    Add metadata for FAQs page

--- app/(pages)/faqs/page.tsx
+++ app/(pages)/faqs/page.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import IntroSection from './Components/IntroSection';
 import FaQsSection from './Components/FaQsSection';
+import { aismMetadata } from "@/lib/Metadata";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    ...aismMetadata,
+    title: "AISM FAQs | Your Questions, Answered",
+    description: "Have questions about AISM? Find answers to frequently asked questions about admissions, academics, student life, and more.",
+    openGraph: {
+        ...aismMetadata.openGraph,
+        title: "AISM FAQs | Your Questions, Answered",
+        description: "Have questions about AISM? Find answers to frequently asked questions about admissions, academics, student life, and more.",
+        url: aismMetadata.openGraph?.url + "/faqs"
+    }
+};
 
 export default function FaqsPage() {
     return (

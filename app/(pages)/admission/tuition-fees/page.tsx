36b3539cfcc95bed1e40ef65dca970e4026cf0de
fabiconcept <favourajokubi@gmail.com>
2024-11-11 12:43:51 +0100

    Add metadata for tuition-fees page

--- app/(pages)/admission/tuition-fees/page.tsx
+++ app/(pages)/admission/tuition-fees/page.tsx
@@ -2,6 +2,21 @@ import { tuitionSteps } from "@/lib/Variables/TuitionFees";
 import Steps from "../General Component/Steps";
 import Top from "../General Component/Top";
 
+import { aismMetadata } from "@/lib/Metadata";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    ...aismMetadata,
+    title: "Invest in Your Future | AISM Tuition & Financial Aid Options",
+    description: "Find detailed information about AISM’s tuition rates, fees, and available financial aid options to help you plan for your medical education.",
+    openGraph: {
+        ...aismMetadata.openGraph,
+        title: "Invest in Your Future | AISM Tuition & Financial Aid Options",
+        description: "Find detailed information about AISM’s tuition rates, fees, and available financial aid options to help you plan for your medical education.",
+        url: aismMetadata.openGraph?.url + "/admission/tuition-fees"
+    }
+};
+
 export default function page() {
     return (
         <div className="mt-44">

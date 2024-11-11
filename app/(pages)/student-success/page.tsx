e1b1d35084885c4ec1fff0915f2c1000e3a06cbf
fabiconcept <favourajokubi@gmail.com>
2024-11-11 12:56:39 +0100

    Add metadata for Student Success page

--- app/(pages)/student-success/page.tsx
+++ app/(pages)/student-success/page.tsx
@@ -1,5 +1,19 @@
 import MiniInfo from "./Components/MiniInfo";
 import Stories from "./Components/Stories";
+import { aismMetadata } from "@/lib/Metadata";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    ...aismMetadata,
+    title: "AISM Student Life | Experience a Vibrant Campus Community",
+    description: "Discover inspiring stories of AISM alumni who have gone on to achieve remarkable success in the medical field. Learn how our rigorous academic programs and supportive community helped shape their futures. Join a legacy of excellence.",
+    openGraph: {
+        ...aismMetadata.openGraph,
+        title: "AISM Student Life | Experience a Vibrant Campus Community",
+        description: "Discover inspiring stories of AISM alumni who have gone on to achieve remarkable success in the medical field. Learn how our rigorous academic programs and supportive community helped shape their futures. Join a legacy of excellence.",
+        url: aismMetadata.openGraph?.url + "/student-success"
+    }
+};
 
 export default function StudentSuccessPage() {
     return (

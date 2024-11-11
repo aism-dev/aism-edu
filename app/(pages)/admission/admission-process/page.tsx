48cac318e7f9707639b449af1ccf517087fae1f7
fabiconcept <favourajokubi@gmail.com>
2024-11-11 12:42:19 +0100

    Add metadata for admission process page

--- app/(pages)/admission/admission-process/page.tsx
+++ app/(pages)/admission/admission-process/page.tsx
@@ -2,6 +2,22 @@ import { admissionSteps } from "@/lib/Variables/AdmissionProcess";
 import Steps from "../General Component/Steps";
 import Top from "../General Component/Top";
 
+
+import { aismMetadata } from "@/lib/Metadata";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    ...aismMetadata,
+    title: "Your Path to AISM | A Step-by-Step Admission Guide",
+    description: "Explore our step-by-step guide to applying at AISM, from initial requirements to enrollment. We make it easy for you to start your journey toward a world-class medical education.",
+    openGraph: {
+        ...aismMetadata.openGraph,
+        title: "Your Path to AISM | A Step-by-Step Admission Guide",
+        description: "Explore our step-by-step guide to applying at AISM, from initial requirements to enrollment. We make it easy for you to start your journey toward a world-class medical education.",
+        url: aismMetadata.openGraph?.url + "/admission/admission-process"
+    }
+};
+
 export default function page() {
     return (
         <div className="mt-44">
